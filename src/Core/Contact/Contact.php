f8a06d79c1147ac556a175418dd2fa309b5cbba7
Dominik Cwiklik <cwiklik@prosoftware.pl>
2015-04-12 23:52:36 +0200

    add comments

--- src/Core/Contact/Contact.php
+++ src/Core/Contact/Contact.php
@@ -5,16 +5,28 @@ use Booking\Util\Collection;
 
 class Contact extends AbstractContact
 {
+    /**
+     *
+     */
     public function __construct()
     {
         $this->fields = new Collection();
     }
 
+    /**
+     * @param $field
+     * @return bool|mixed
+     */
     public function get($field)
     {
         return $this->fields->getByKey($field);
     }
 
+    /**
+     * @param $value
+     * @param $field
+     * @param null $subkey
+     */
     public function set($value, $field, $subkey = null)
     {
         $this->fields->add($value, $field);
